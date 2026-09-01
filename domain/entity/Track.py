416de244075e11586777e12c646c48d97e60cb2c
Arthur Tomazelli de Campos <arthurtomazelli22@gmail.com>
2026-09-01 13:03:26 -0300

    feat: implement Pitch and refactor entities that use it

--- domain/entity/Track.py
+++ domain/entity/Track.py
@@ -2,14 +2,14 @@ from typing import List
 
 from domain.entity.Measure import Measure
 from domain.enum.InstrumentTuning import InstrumentTuning
-from domain.constant.CHROMATIC_SCALE import chromatic_scale
+from domain.value_object.Pitch import Pitch
 
 
 class Track:
     def __init__(self, name: str, instrument: InstrumentTuning):
         self.name = name
         self.instrument = instrument
-        self.tuning: List[str] = instrument.value.copy()
+        self.tuning: List[Pitch] = instrument.value.copy()
         self.measures: List[Measure] = []
 
     def down_tune(self):
@@ -18,15 +18,12 @@ class Track:
     def up_tune(self):
         self.change_tuning(1)
 
-    def change_tuning(self, change: int):
-        for i, note in enumerate(self.tuning):
-            self.shift_note(i, note, change)
+    def change_tuning(self, shift_amount: int):
+        for i, pitch in enumerate(self.tuning):
+            self.tuning[i] = Pitch.shift(pitch, shift_amount)
 
     def drop_tune(self):
         last_string_index = len(self.tuning) - 1
-        note = self.tuning[last_string_index]
+        pitch = self.tuning[last_string_index]
 
-        self.shift_note(last_string_index, note, -2)
-
-    def shift_note(self, index: int, note: str, shift: int):
-        self.tuning[index] = chromatic_scale[(chromatic_scale.index(note) + shift) % 12]
+        self.tuning[last_string_index] = Pitch.shift(pitch, -2)
